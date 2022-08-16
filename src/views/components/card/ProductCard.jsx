419f03933bc6fea5b07f4cd54436968d13fc9aed
mainul163 <mainulisraf163@gmail.com>
2022-08-16 13:08:02 +0600

    card issues slovled

--- src/views/components/card/ProductCard.jsx
+++ src/views/components/card/ProductCard.jsx
@@ -10,12 +10,12 @@ import { addToCartList, decrementQty, incrementQty } from '../../../redux/slice/
 import { Product } from '../../../services';
 import { MinusIcon, PlusIcon } from '@heroicons/react/solid';
 import QuickView from '../overly/QuickView';
-
+import './ProductCard.css'
 const ProductCard = ({ item }) => {
     const [open, setOpen] = useState(false)
     return (
         <>
-            <div className='bg-white rounded-lg hover:bg-green-200 shadow-md group transition-all duration-300 ease-linear h-full flex flex-col'>
+            <div className='bg-white rounded-lg hover:bg-green-200 cardShadow group transition-all duration-300 ease-linear h-full flex flex-col mt-5'>
                 <div className="w-full h-[145px] md:h-[273px] relative p-2 overflow-hidden">
                     <Link to={'/product/' + item?.slug}>
                         <img className='w-full h-full bg-white' src={productImg + item?.images[0]} alt="" />
@@ -23,7 +23,7 @@ const ProductCard = ({ item }) => {
                     {parseInt(item?.discount_amount) !== 0 && <div className="absolute top-2 right-2 bg-[#ff576d] px-[7px] py-[2px] w-[75px] h-[22px] rounded-md text-white flex justify-center items-center text-xs font-semibold">
                         {bangla ? `${parseInt(item?.discount_amount)}% ছাড়` : `${parseInt(item?.discount_amount)}% OFF`}
                     </div>}
-                    <button onClick={() => setOpen(true)} className='absolute -bottom-2 translate-y-6 group-hover:translate-y-0 transition-all duration-300 ease-linear left-0 right-0 mx-auto rounded-t-md px-1 font-semibold text-md pb-2 text-white text-md flex justify-center items-center gap-x-1 shadow-4xl bg-[#4c9a2a]'>Quick View</button>
+                    <button onClick={() => setOpen(true)} className='absolute -bottom-3 translate-y-6 group-hover:translate-y-0 transition-all duration-300 ease-linear left-0 right-0 mx-auto rounded-t-md px-1 font-semibold text-md pb-2 text-white text-md flex justify-center items-center gap-x-1 shadow-4xl bg-[#4c9a2a]'>Quick View</button>
                 </div>
                 <div className="h-[1px] w-full my-2 group-hover:bg-green-200 bg-gray-200 transition-all duration-300 ease-linear"></div>
                 <div className="p-2 h-[80px]">
@@ -32,7 +32,8 @@ const ProductCard = ({ item }) => {
                 </div>
                 <div className="h-[50px] flex flex-col justify-end">
                     {item?.discount_price && <div className="text-[12px] px-2 line-through">
-                        <Taka tk={item?.regular_price} className={`text-[#d4463c]`} size={17} />
+                    
+                        <Taka tk={item?.regular_price} className={`text-[#d4463c] font-bold`} size={17} />
                     </div>}
                     <div className="">
                         <AddToCart item={item} />
@@ -92,7 +93,7 @@ const AddToCart = ({ item }) => {
         <div className="flex justify-between items-center px-2 pb-2">
 
             <div className='text-[13px]'>
-                <Taka tk={item?.discount_price ? item?.discount_price : item?.regular_price} className={`text-[#4c9a2a]`} size={23} />
+                <Taka tk={item?.discount_price ? item?.discount_price : item?.regular_price} className={`text-[#4c9a2a] font-bold`} size={18} />
             </div>
             <div className="rounded-full border border-[#4c9a2a] hover:bg-[#4c9a2a] stroke-[#4c9a2a] text-[#4c9a2a] hover:text-white hover:stroke-white transition-all duration-300 ease-linear">
 
@@ -110,8 +111,8 @@ const AddToCart = ({ item }) => {
                     </div>
                     :
                     <div onClick={() => handleCart()} className="flex items-center gap-2 cursor-pointer px-2">
-                        <ShoppingBagIcon className='h-4 w-4  stroke-2 ' />
-                        <p className='font-semibold  text-sm'>{bangla ? "কার্ট" : "Cart"}</p>
+                        <ShoppingBagIcon className='h-4 w-4  stroke-2 font-bold' />
+                        <p className=' font-bold text-sm'>{bangla ? "কার্ট" : "Cart"}</p>
                     </div>
                 }
             </div>
