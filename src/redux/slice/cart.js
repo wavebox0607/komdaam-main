5ca0a7719888dbd3de2bf88dba25263268d0b579
Mainul islam <mainulisraf163@gmail.com>
2022-08-22 23:13:39 +0600

    update done

--- src/redux/slice/cart.js
+++ src/redux/slice/cart.js
@@ -1,6 +1,5 @@
-import { createSlice } from '@reduxjs/toolkit'
-import { toast } from 'react-toastify';
-
+import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
 
 export const cartSlice = createSlice({
   name: "cart",
@@ -10,83 +9,88 @@ export const cartSlice = createSlice({
   },
   reducers: {
     addToCartList: (state, action) => {
-      
-      const cartItem = state.cartList?.find((item) => item.id === action.payload.id && item.productId === action.payload.productId && item.id === action.payload.id)
-      
+      // const cartItem = state.cartList?.find((item) => item.id === action.payload.id & item.color === action.payload.color && item.size === action.payload.size);
+      const cartItem = state.cartList?.find(
+        (item) =>
+          item.id === action.payload.cartId &&
+          item.size === action.payload.size &&
+          item.color === action.payload.color &&
+          item.unit === action.payload.unit &&
+          item.volume === action.payload.volume
+      );
+
       if (cartItem) {
-       
-        cartItem.qty = action.payload.qty ? cartItem.qty + action.payload.qty : cartItem.qty + 1;
-        state.cartList.push({ ...action.payload, qty: action.payload.qty || 1 });
-        toast("Add to cart z2 successfully!", {
-          type: 'success'
-        })
+        cartItem.qty = action.payload.qty
+          ? cartItem.qty + action.payload.qty
+          : cartItem.qty + 1;
       } else {
-        console.log('tt',action);
-        state.cartList.push({ ...action.payload, qty: action.payload.qty || 1 });
-        toast("Add to cart z1 successfully!", {
-          type: 'success'
-        })
+        state.cartList.push({
+          ...action.payload,
+          qty: action.payload.qty || 1,
+        });
       }
     },
     incrementQty: (state, action) => {
-      const cartItem = state.cartList?.find((item) => item.cartId === action.payload);
+      const cartItem = state.cartList?.find(
+        (item) => item.cartId === action.payload
+      );
       if (cartItem) {
         cartItem.qty = cartItem.qty + 1;
         toast("Add to cart z successfully!", {
-          type: 'success'
-        })
+          type: "success",
+        });
       }
     },
 
     decrementQty: (state, action) => {
-      const cartItem = state.cartList.find((item) => item.cartId === action.payload);
-
+      const cartItem = state.cartList.find(
+        (item) => item.cartId === action.payload
+      );
 
       if (cartItem.qty === 1) {
-        state.cartList = state.cartList.filter(i => i.cartId !== action.payload)
+        state.cartList = state.cartList.filter(
+          (i) => i.cartId !== action.payload
+        );
         toast("Remove from cart successfully!", {
-          type: 'warning'
-        })
+          type: "warning",
+        });
       }
 
       if (cartItem.qty > 1) {
         cartItem.qty = cartItem.qty - 1;
         toast("Remove from cart successfully!", {
-          type: 'warning'
-        })
+          type: "warning",
+        });
       }
     },
 
-    // Remove single product from cart List 
+    // Remove single product from cart List
     removeToCartList: (state, action) => {
-      state.cartList = state.cartList.filter((items) => items.cartId !== action.payload)
+      state.cartList = state.cartList.filter(
+        (items) => items.cartId !== action.payload?.cartId
+      );
     },
 
-    // delete all product in cart List 
+    // delete all product in cart List
     clearCartList: (state) => {
-      state.cartList = []
+      state.cartList = [];
     },
 
-
-
-
-    // Right side Cart open / close 
+    // Right side Cart open / close
     toggleCart: (state) => {
-      state.cartOpen = !state.cartOpen
+      state.cartOpen = !state.cartOpen;
     },
-
-
-
-
-
   },
+});
 
-})
+export const {
+  addToCartList,
+  removeToCartList,
+  incrementQty,
+  decrementQty,
+  clearCartList,
+  toggleCart,
+  setLanguage,
+} = cartSlice.actions;
 
-
-export const { addToCartList, removeToCartList, incrementQty, decrementQty, clearCartList, toggleCart, setLanguage } = cartSlice.actions
-
-
-export default cartSlice.reducer
-
-
+export default cartSlice.reducer;
--- src/views/components/common/RightSide.jsx
+++ src/views/components/common/RightSide.jsx
@@ -1,82 +1,133 @@
- import { ChevronDownIcon, ChevronUpIcon, ShoppingCartIcon, XIcon } from '@heroicons/react/solid';
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link, NavLink } from 'react-router-dom';
-import { productImg } from '../../../constant/imgUri';
-import { bangla } from '../../../constant/language';
-import { decrementQty, incrementQty, removeToCartList, toggleCart } from '../../../redux/slice/cart';
-import { Taka } from '../utils';
-import HomePage from './../../../services/home.service';
+import {
+  ChevronDownIcon,
+  ChevronUpIcon,
+  ShoppingCartIcon,
+  XIcon,
+} from "@heroicons/react/solid";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, NavLink } from "react-router-dom";
+import { productImg } from "../../../constant/imgUri";
+import { bangla, handleLanguage } from "../../../constant/language";
+import {
+  decrementQty,
+  incrementQty,
+  removeToCartList,
+  toggleCart,
+} from "../../../redux/slice/cart";
+import { Taka } from "../utils";
+import HomePage from "./../../../services/home.service";
 
 const RightSide = ({ right }) => {
-    const dispatch = useDispatch()
-    const { data } = HomePage.GetSettings()
-    const { cartList } = useSelector((state) => state.cart)
-    const priceList = cartList?.map(p => p.qty * p.price)
-    const total = priceList.reduce(
-        (previousValue, currentValue) => previousValue + currentValue,
-        0
-    );
-   
-    return (
-        <div className={`hidden 2md:block fixed right-0 top-[70px] bottom-0    ${right ? "w-[330px]" : "w-[0px]"} transition-all duration-100 ease-linear !z-[10] shadow-lg`}>
-            <div className="flex flex-col justify-between h-full ">
-                <div className="flex-1 w-full overflow-y-scroll example">
-                    <div className="flex justify-between bg-[#4c9a2a] px-2 h-[40px] items-center">
-                        <div className="flex items-center text-[15px] font-bold text-white gap-2">
-                            <ShoppingCartIcon className='h-5 w-5 ' color='white' />
-                            <p>{cartList.length + " "}{bangla ? "আইটেম" : "ITEMS"}</p>
-                        </div>
-                        <div className="">
-                            <button onClick={() => dispatch(toggleCart())} className='border border-white px-4 hover:text-[#4c9a2a] text-white hover:bg-gray-50 transition-all duration-300 ease-linear py-0'>{bangla ? "বন্ধ" : "close"}</button>
-                        </div>
-                    </div>
-                    <h3 className='bg-gray-50 font-semibold py-1 ml-2'>{bangla ? "দ্রুত ডেলিভারী" : "Express delivery"}</h3>
-                    {
-                        cartList?.map((item) => <SingleCart key={item?.cartId} item={item} />)
-                    }
-                </div>
-                <div className=" w-full mb-3">
-                    <NavLink to='/checkout' className="flex mx-6 rounded-lg text-white overflow-hidden">
-                        <div className="flex justify-center items-center bg-[#ff9eab] font-semibold flex-1 py-2">{bangla ? "অর্ডার করুন" : "Checkout"}</div>
-                        <div className="flex justify-center items-center bg-[#ff7358] flex-1 py-2"><Taka tk={total} /></div>
-                    </NavLink>
-                </div>
-                <div className=' mx-6 text-lg text-[#4aa02c] font-bold text-center pb-2'>
-                    Phone : {data?.settings?.phone}
-                </div>
+  const dispatch = useDispatch();
+  const { data } = HomePage.GetSettings();
+  const { cartList } = useSelector((state) => state.cart);
+  const priceList = cartList?.map((p) => p.qty * p.price);
+  const total = priceList.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    0
+  );
+
+  return (
+    <div
+      className={`hidden 2md:block fixed right-0 top-[70px] bottom-0    ${
+        right ? "w-[330px]" : "w-[0px]"
+      } transition-all duration-100 ease-linear !z-[10] shadow-lg`}
+    >
+      <div className="flex flex-col justify-between h-full ">
+        <div className="flex-1 w-full overflow-y-scroll example">
+          <div className="flex justify-between bg-[#4c9a2a] px-2 h-[40px] items-center">
+            <div className="flex items-center text-[15px] font-bold text-white gap-2">
+              <ShoppingCartIcon className="h-5 w-5 " color="white" />
+              <p>
+                {cartList.length + " "}
+                {bangla ? "আইটেম" : "ITEMS"}
+              </p>
             </div>
+            <div className="">
+              <button
+                onClick={() => dispatch(toggleCart())}
+                className="border border-white px-4 hover:text-[#4c9a2a] text-white hover:bg-gray-50 transition-all duration-300 ease-linear py-0"
+              >
+                {bangla ? "বন্ধ" : "close"}
+              </button>
+            </div>
+          </div>
+          <h3 className="bg-gray-50 font-semibold py-1 ml-2">
+            {bangla ? "দ্রুত ডেলিভারী" : "Express delivery"}
+          </h3>
+          {cartList?.map((item) => (
+            <SingleCart key={item?.cartId} item={item} />
+          ))}
         </div>
-    );
+        <div className=" w-full mb-3">
+          <NavLink
+            to="/checkout"
+            className="flex mx-6 rounded-lg text-white overflow-hidden"
+          >
+            <div className="flex justify-center items-center bg-[#ff9eab] font-semibold flex-1 py-2">
+              {bangla ? "অর্ডার করুন" : "Checkout"}
+            </div>
+            <div className="flex justify-center items-center bg-[#ff7358] flex-1 py-2">
+              <Taka tk={total} />
+            </div>
+          </NavLink>
+        </div>
+        <div className=" mx-6 text-lg text-[#4aa02c] font-bold text-center pb-2">
+          Phone : {data?.settings?.phone}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RightSide;
 
-
-
 const SingleCart = ({ item }) => {
-    const dispatch = useDispatch()
-    return (
-        <div className="flex justify-between items-center py-3 border-b border-gray-300 last:border-0">
-            <div className="flex flex-col space-y-2 items-center w-[10px] h-full ml-3">
-                <div onClick={() => dispatch(incrementQty(item?.cartId))} className=""><ChevronUpIcon className='h-4 w-4' /></div>
-                <div className="text-xs">{item?.qty}</div>
-                <div onClick={() => dispatch(decrementQty(item?.cartId))} className=""><ChevronDownIcon className='h-4 w-4' /></div>
-            </div>
-            <div className="h-[40px] w-[40px] flex items-center">
-                <img src={productImg + item?.images[0]} alt="" />
-            </div>
-            <div className="flex flex-col w-[120px] ">
-                <Link to={'/product/' + item?.slug}><div className="text-[13px] text-[#1d1d1d] text-justify w-full" style={{ height: '30px', overflow: 'hidden', whiteSpace: 'nowrap', width: '130px', textOverflow: 'ellipsis' }}>{item?.name.slice(0, 30)}</div></Link>
-                <Taka className={"text-[10px]"} tk={item?.price} />
-            </div>
-            <div className="flex flex-col justify-center w-[63px]">
+  const dispatch = useDispatch();
+  const handle = (item) => {
+    console.log("id", item);
+  };
 
-                <Taka tk={item?.price * item?.qty} />
-            </div>
-            <div className="flex flex-col justify-center items-center w-[10px] mr-4">
-                <div onClick={() => dispatch(removeToCartList(item?.cartId))} className=""><XIcon className='h-4 w-4' /></div>
-            </div>
+  return (
+    <div className="flex justify-between items-center py-3 border-b border-gray-300 last:border-0">
+      <div className="flex flex-col space-y-2 items-center w-[10px] h-full ml-3">
+        <div onClick={() => dispatch(incrementQty(item?.cartId))} className="">
+          <ChevronUpIcon className="h-4 w-4" />
         </div>
-    )
-}
+        <div className="text-xs">{item?.qty}</div>
+        <div onClick={() => dispatch(decrementQty(item?.cartId))} className="">
+          <ChevronDownIcon className="h-4 w-4" />
+        </div>
+      </div>
+      <div className="h-[40px] w-[40px] flex items-center">
+        <img src={productImg + item?.images[0]} alt="" />
+      </div>
+      <div className="flex flex-col w-[120px] ">
+        <Link to={"/product/" + item?.slug}>
+          <div
+            className="text-[13px] text-[#1d1d1d] text-justify w-full"
+            style={{
+              height: "30px",
+              overflow: "hidden",
+              whiteSpace: "nowrap",
+              width: "130px",
+              textOverflow: "ellipsis",
+            }}
+          >
+            {item?.name.slice(0, 30)}
+          </div>
+        </Link>
+        <Taka className={"text-[10px]"} tk={item?.price} />
+      </div>
+      <div className="flex flex-col justify-center w-[63px]">
+        <Taka tk={item?.price * item?.qty} />
+      </div>
+      <div className="flex flex-col justify-center items-center w-[10px] mr-4">
+        <div onClick={() => dispatch(removeToCartList(item))} className="">
+          <XIcon className="h-4 w-4" />
+        </div>
+      </div>
+    </div>
+  );
+};
