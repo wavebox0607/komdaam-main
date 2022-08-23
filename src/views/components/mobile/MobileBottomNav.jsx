c1dafb8bba9a84d3d0dd730396b03dfaa3ccf149
mainul163 <mainulisraf163@gmail.com>
2022-08-23 13:37:02 +0600

    favicon  update

--- src/views/components/mobile/MobileBottomNav.jsx
+++ src/views/components/mobile/MobileBottomNav.jsx
@@ -22,7 +22,7 @@ const MobileBottomNav = ({ cart, setCart, setCategory, category }) => {
 
     return (
         <>
-            <div className='fixed bottom-0 left-0 w-full px-8 py-2 bg-red-200 shadow-lg 2md:hidden block !z-[100]'>
+            <div className='fixed bottom-0 left-0 w-full px-8 py-2 bg-white shadow-lg 2md:hidden block !z-[100]'>
                 <div className="flex justify-between py-2">
                     {category ? <div onClick={handleCategory} className="text-black font-semibold text-lg">
                         <IoMenuOutline className='text-[28px]' />
@@ -35,7 +35,7 @@ const MobileBottomNav = ({ cart, setCart, setCategory, category }) => {
                         </div>
                     </NavLink>
 
-                    <a href='tel:+8801677515579' onClick='console.log("The link was clicked."); return false' >
+                    <a href='tel:+8801865460756' onClick='console.log("The link was clicked."); return false' >
                         <div className="text-black font-semibold text-lg p-3 rounded-full border absolute bottom-6 left-[46%] -translate-x-[20%]  bg-[#4c9a2a]">
                             <PhoneIcon className='w-7 h-7 text-white' />
 
