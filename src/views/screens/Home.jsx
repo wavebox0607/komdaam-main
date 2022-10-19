1101cbf7b83cdb509ccd5cd5a88c108454fd1e91
wavebox0607 <wavebox.0607@gmail.com>
2022-10-19 12:47:06 +0600

    offer upadte

--- src/views/screens/Home.jsx
+++ src/views/screens/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Banner, CategoryProduct, CurrentOffer, FeatureCategory, FeatureProduct, Help, Hero, Step } from '../components/screen/home';
+// import { Banner, CategoryProduct, CurrentOffer, FeatureCategory, FeatureProduct, Help, Hero, Step } from '../components/screen/home';
+import { Banner, CategoryProduct, CurrentOffer, FeatureCategory, Help, Hero, Step } from '../components/screen/home';
 import BannerOne from '../components/screen/home/BannerOne';
 import BannerTwo from '../components/screen/home/BannerTwo';
 import Unit from './Unit';
