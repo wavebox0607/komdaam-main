d05659369eb003a5e2fd5231bcbc875dca5c4b60
wavebox0607 <wavebox.0607@gmail.com>
2022-10-22 10:58:31 +0600

    update

--- src/views/components/screen/home/BannerTwo.jsx
+++ src/views/components/screen/home/BannerTwo.jsx
@@ -18,7 +18,7 @@ const BannerTwo = () => {
 
 
                 <div className="mt-3 shadow-lg rounded-md grid md:grid-cols-4 grid-cols-2 gap-x-3 gap-y-3 px-4">
-                    {data?.double_banner?.slice(4,8).map((b) => <img key={b?.id} className='w-[100%] h-[190px] md:h-[280px]' src={bannerImg + b?.image} alt="" />)}
+                    {data?.double_banner?.slice(4,8).map((b) => <img key={b?.id} className='w-[100%] h-[190px] md:h-[260px]' src={bannerImg + b?.image} alt="" />)}
 
                 
 
