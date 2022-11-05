f7f41938ab77945960a7a64ebe3ec06043e2a3b0
mainul163 <mainulisraf163@gmail.com>
2022-11-05 16:23:30 +0600

    done

--- src/views/components/mobile/MobileBottomNav.jsx
+++ src/views/components/mobile/MobileBottomNav.jsx
@@ -19,7 +19,9 @@ const MobileBottomNav = ({ cart, setCart, setCategory, category }) => {
         setCategory(!category)
     }
 
-
+const phnftn=()=>{
+    window.ReactNativeWebView.postmessage({message: "Message here"});
+}
     return (
         <>
             <div className='fixed bottom-0 left-0 w-full px-8 py-2 bg-white shadow-lg 2md:hidden block !z-[100]'>
@@ -35,12 +37,19 @@ const MobileBottomNav = ({ cart, setCart, setCategory, category }) => {
                         </div>
                     </NavLink>
 
-                    <a href='tel:+8801865460756' onClick='console.log("The link was clicked."); return false' >
+                    {/* <a href='tel:+8801865460756' onClick='console.log("The link was clicked."); return false' >
                         <div className="text-black font-semibold text-lg p-3 rounded-full border absolute bottom-6 left-[46%] -translate-x-[20%]  bg-[#4c9a2a]">
                             <PhoneIcon className='w-7 h-7 text-white' />
 
                         </div>
-                    </a>
+                    </a> */}
+
+                    
+                        <div className="text-black font-semibold text-lg p-3 rounded-full border absolute bottom-6 left-[46%] -translate-x-[20%]  bg-[#4c9a2a]" onClick={phnftn}>
+                            <PhoneIcon className='w-7 h-7 text-white' />
+
+                        </div>
+               
                     {cart ? <div className=" items-center text-black font-semibold text-lg">
                         <ShoppingOutline className='  w-7 h-7' />
                     </div> :
